6927281e1468c0aba485d8ec260ca2328ac20666
Do Minh Hieu <minhhieudo.it@gmail.com>
2021-09-15 06:48:58 +0700

    Bai4_ComparisonOperator

---
+++ Bai4_ComparisonOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Bai4_ComparisonOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // phép toán so sánh // == > < >= <= !=
+            // bool isOnline ;
+            // isOnline = true ; 
+            // isOnline = false; 
+            int a = 5 ; 
+            int b = 6 ; 
+            bool kq = a == b ; 
+           // Console.WriteLine(kq);
+            // chèn biểu thức vào chuỗi 
+            Console.WriteLine($"a != b ==> {a != b}");
+            // && cả a và b đều đúng = > true
+            // || 1 trong 2 đúng => đúng 
+        }
+    }
+}
