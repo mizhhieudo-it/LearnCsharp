a2d42f18c99b547a0de4720f70b4f21e251b4d63
Do Minh Hieu <minhhieudo.it@gmail.com>
2021-09-15 06:37:46 +0700

    upload CS003-Operator Csharp

---
+++ Bai3_OperatorInCsharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bai3_OperatorInCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Operator + - x % 
+            float a = 5 ;
+            int b = 4 ; 
+            Console.WriteLine("a + b = {0}",a +b )  ;
+            Console.WriteLine("a - b = {0}",a - b )  ;
+            Console.WriteLine("a * b = {0}",a *b )  ;
+            Console.WriteLine("a / b = {0}",a /b )  ;
+            Console.WriteLine("a % b = {0}",a % b )  ;
+        }
+    }
+}
