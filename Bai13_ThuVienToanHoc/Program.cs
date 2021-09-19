0199ebce65853eacf270a48e8c4d195d578fa7aa
Do Minh Hieu <minhhieudo.it@gmail.com>
2021-09-19 16:36:47 +0700

    Bai13_Math in Cshap

---
+++ Bai13_ThuVienToanHoc/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Bai13_ThuVienToanHoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // hafm pi
+            Console.WriteLine($"Hang so PI{Math.PI}");
+            Console.WriteLine("Hello World!");
+            // ham max , min
+            //Abs - trị tuyệt đối  ,Sign (  return if value = 0 ; return 1 if value > 0 ; return - 1 if value <0) 
+            //Luong giac 
+            // Console($"sin:{Math.Sin(Math.PI/4)}");
+         // tính căn sqrt(a) , pow(a,b) : a mũ b 
+         // làm tròn :
+         // Math.Round() : làm tròn số nguyên gần nhất 
+         // Math.ceiling() làm tròn lên : làm tròn số đó >=a 
+         // Math.Floor() làm tròn xuống :
+         // Math.Trancate(a): cắt đi phần thập phân lấy 0.4 -> 0 / 0.6 -> 1
+        }
+    }
+}
