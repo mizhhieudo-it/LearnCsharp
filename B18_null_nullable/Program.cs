e6856e2b3942f1a69164be2331dee33cfedd596d
Do Minh Hieu <minhhieudo.it@gmail.com>
2021-09-24 14:29:04 +0700

    Bai18_Null_Nullable

---
+++ B18_null_nullable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace B18_null_nullable
+{
+    class Abc{
+        public void XinChao() => Console.WriteLine("it's oke");
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {// null,bullable: từ khóa null sẽ gán cho tham chiếu , nếu bắt buộc cần tham trị mà gán null ta cho int? trước trường 
+            Abc a = new Abc();
+            if(a !=null){
+                a.XinChao();
+            }
+            a?.XinChao(); // nếu a != thực hiện xin chào
+            // khai báo kiểu tham trị mà nhận null 
+            int? age ; 
+            age = null ; 
+            age = 100 ;
+            if(age.HasValue){ //hasvalue nghĩa là = true nếu có đc gán và flase nếu ko đc gán 
+                int _age = age.Value;
+                Console.WriteLine(_age);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
